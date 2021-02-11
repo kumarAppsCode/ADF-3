8b5130cfe148ed3227e177a913de16e0ce109817
Dinesh <pydinesh3@gmail.com>
2021-02-11 19:11:18 +0530

    Create Base.java

---
+++ YSpring/Code/GetData/Base.java
@@ -0,0 +1,53 @@
+
+
+
+***************************************************************************************************************************
+   // Get All Record
+    @RequestMapping("/function")
+    public List<Function_EO> getFunction() {
+        return (List<Function_EO>) function_SO.getAllFunction();
+    }
+===========================================================
+        public List<Function_EO> getAllFunction() {
+        List<Function_EO> getAllfunls = new ArrayList<>();
+        function_RO.findAll().forEach(getAllfunls::add);
+        return getAllfunls;
+    }
+***************************************************************************************************************************
+    //Path Variable to find 1 records--Default
+    @RequestMapping(value = "/function/{fun_id}")
+    public Optional<Function_EO> getFunctionById(@PathVariable("fun_id") Integer function_id) {
+        return function_SO.findFunctionById(function_id);
+    }
+===========================================================
+    // find by Id
+    public Optional<Function_EO> findFunctionById(Integer function_id) {
+        return function_RO.findById(function_id);
+    }
+***************************************************************************************************************************    
+    // Get All - Request param
+    @RequestMapping("/functionName")
+    public List<Function_EO> getFunction(@RequestParam(value = "fun_name", required = false) String funName) {
+        return (List<Function_EO>) function_SO.getAllFunctionName(funName);
+    }
+===========================================================    
+    public List<Function_EO> getAllFunctionName(String funName) {
+        if (funName == null) {
+            List<Function_EO> getAllfunls = new ArrayList<>();
+            function_RO.findAll().forEach(getAllfunls::add);
+            return getAllfunls;
+        } else {
+            return function_RO.findAllByfuncnameContains(funName);
+            // return function_jpa_rO.findAllByfunc_name(funName);
+        }
+    }
+    List<Function_EO> findAllByfuncnameContains(String funname); --- in Repo
+***************************************************************************************************************************    
+
+
+
+
+
+
+
+
