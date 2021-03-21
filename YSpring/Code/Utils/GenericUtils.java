4f39a82a354f90b036b7bd24ea2055fa35ed405e
Dinesh <pydinesh3@gmail.com>
2021-03-21 13:35:03 +0530

    Update GenericUtils.java

--- YSpring/Code/Utils/GenericUtils.java
+++ YSpring/Code/Utils/GenericUtils.java
@@ -1,4 +1,8 @@
-    public static String getCurrentdate() {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public static String getCurrentdate() {
         Date date = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
         String strDate = formatter.format(date);
