93900c1c4ab023668b5bf1cded5fe226ae0e825a
Dinesh <pydinesh3@gmail.com>
2021-05-23 22:44:25 +0530

    Update 01httServer.js

--- NodeJs/03Learn/Base/01httServer.js
+++ NodeJs/03Learn/Base/01httServer.js
@@ -8,3 +8,6 @@ const server=http.createServer((req, res)=>{
 });
 
 server.listen(3000);
+*******************************************************
+ Life Cycle:
+app.js run--->start--->script/var/function--->server start-->loop (loop event listener)
