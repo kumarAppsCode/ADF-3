17284756f8ddf3c2a56088f1551243352d3f0511
Dinesh <pydinesh3@gmail.com>
2021-06-03 13:16:00 +0530

    Update 01 SimplePost.java

--- YSpring/Code/ResponseEntity/POST/01 SimplePost.java
+++ YSpring/Code/ResponseEntity/POST/01 SimplePost.java
@@ -14,3 +14,30 @@
         return ResponseEntity.ok(ls);
     }
 ---------------------------------------------------------------------------------------------------------------------
+    @Repository
+public interface Contract_RP extends JpaRepository<Contract_EO ,BigDecimal> {
+    
+}
+---------------------------------------------------------------------------------------------------------------------
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "XXSM_CONTRACT_HEADER")
+// @JsonInclude(JsonInclude.Include.NON_NULL)
+public class Contract_EO {
+
+@Id
+@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "contract_hdr_id_seq")
+@SequenceGenerator(name = "contract_hdr_id_seq",sequenceName = "contract_hdr_id_s", allocationSize = 1)
+@Column(name = "CONTRACT_ID")
+public BigDecimal contractId;
+@Column(name = "CONTRACT_NUMBER")
+public String contractNumber;
+@Column(name = "CONTRACT_NAME")
+public String contractName;
+@Column(name = "CONTRACT_DATE")
+@JsonFormat(pattern="dd-MM-YYYY")
+public Date contractDate;
