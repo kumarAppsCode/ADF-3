836e354f4517770a817640887b50dde2c701542a
Dinesh <pydinesh3@gmail.com>
2021-06-15 19:30:17 +0530

    Update SpringFoxConfig.java

--- YSpring/Code/Utils/SpringFoxConfig.java
+++ YSpring/Code/Utils/SpringFoxConfig.java
@@ -1,12 +1,5 @@
 Springfox spring starter
-1. MVN: springfox-boot-starter(Parent)
-<!-- https://mvnrepository.com/artifact/io.springfox/springfox-boot-starter -->
-<dependency>
-    <groupId>io.springfox</groupId>
-    <artifactId>springfox-boot-starter</artifactId>
-    <version>3.0.0</version>
-</dependency>
-Child:
+1. MVN: 
 	<dependency>
 		<groupId>io.springfox</groupId>
 		<artifactId>springfox-swagger2</artifactId>
@@ -19,18 +12,19 @@ Child:
 		<version>2.9.2</version>
 	</dependency>
 --------------------------------------------------------------------
-2. Main application : @EnableSwagger2
-3. http://localhost:8081/swagger-ui/
+3. 	http://localhost:8081/swagger-ui.html
+	http://localhost:8081/v2/api-docs
 ***************************************************************************************************
-package com.sample.contract.Utils;
+package com.spring.contractvb.Utils;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import io.swagger.annotations.Api;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -40,14 +34,10 @@ import springfox.documentation.builders.ApiInfoBuilder;
  * @author Dinesh
  *
  */
+
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
-    // http://localhost:8081/swagger-ui/
-
-    // import io.swagger.annotations.Api;
-    // @Api(tags = {"Employees"}, description = "Employee based services for Ooredoo HRMS Mobile Application", 
-    // value = "HRMS Module", produces = "application/json")
 
     @Bean
     public Docket api() { 
@@ -66,10 +56,51 @@ public class SpringFoxConfig {
         .description("List of all the APIs of TEST Mobile App");
         return builder.build();
         }
+
 }
+*********************************************************************************
+import io.swagger.annotations.Api;
 
+@RestController
+@RequestMapping(value = "/module")
+@Api(
+    tags = {"Contract"}, 
+    description = "Contract based REST services Application", 
+    // value = "HRMS Module", 
+    produces = "application/json"
+    )
+*********************************************************************************
+@ApiOperation(value = "Returns all list of users.", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
+    @GetMapping(value = "/getAllUsers", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<User>> getAllUsers() {
 
-*********
-In main class
-@EnableSwagger2
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+        return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
+    }
+*********************************************************************************
+weglogic.xml:-
+<?xml version="1.0" encoding="UTF-8"?>
+<wls:weblogic-web-app
+        xmlns:wls="http://xmlns.oracle.com/weblogic/weblogic-web-app"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://xmlns.oracle.com/weblogic/weblogic-web-app
+        http://xmlns.oracle.com/weblogic/weblogic-web-app/1.4/weblogic-web-app.xsd">
+     <wls:context-root>/apps</wls:context-root>
+    <wls:container-descriptor>
+        <wls:prefer-application-packages>
+            <package-name>org.springframework.*</package-name>
+            <!-- <package-name>org.hibernate.*</package-name> -->
+            <!-- <package-name>javax.validation.*</package-name> -->
+            <!-- <package-name>javax.validation.spi.*</package-name> -->
+            <!-- <package-name>org.slf4j.*</package-name> -->
+            <package-name>com.fasterxml.jackson</package-name>
+        </wls:prefer-application-packages>
+    </wls:container-descriptor>
+ </wls:weblogic-web-app>
+ *********************************************************************************
