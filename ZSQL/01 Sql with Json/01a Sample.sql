f01fffcd940e98f132b639609b770a140cbb8065
Dinesh <pydinesh3@gmail.com>
2021-10-27 19:56:25 +0530

    Create 01a Sample.sql

---
+++ ZSQL/01 Sql with Json/01a Sample.sql
@@ -0,0 +1,28 @@
+declare
+    sample_json   varchar2 (32767)
+        := '{
+    "glossary": {
+        "title": "example glossary",
+        "GlossDiv": {
+            "title": "S",
+            "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+                    "SortAs": "SGML",
+                    "GlossTerm": "Standard Generalized Markup Language",
+                    "Acronym": "SGML",
+                    "Abbrev": "ISO 8879:1986",
+                    "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso": ["GML", "XML"]
+                    },
+                    "GlossSee": "markup"
+                }
+            }
+        }
+    }
+}';
+begin
+    apex_json.parse (sample_json);
+	dbms_output.put_line (apex_json.get_varchar2 ('glossary.title'));
+end;
